31bf5d36d270efd4b9162170ba53a4edc810dcbd
sc-zw <163466197+sc-zw@users.noreply.github.com>
2025-11-19 14:10:41 -0500

    Update global.js

--- global.js
+++ global.js
@@ -6,6 +6,8 @@ plot.style.top = "50px";
 plot.style.fontSize = "20px";
 //let flat = "\u2610";//
 let flat = " ";
+
+let chance = 0.01;
 let fog = "\u2592";
 let hill = "\u039B"
 let A = flat + fog + hill;
